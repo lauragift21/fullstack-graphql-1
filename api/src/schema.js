6f34d553e983f8a6aef2e709546ff900af13a860
lauragift21 <laurandidi21@gmail.com>
2019-12-31 07:32:56 +0100

    Add input type exercise

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,10 +1,32 @@
 const { gql } = require('apollo-server')
 
+
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  input PetInput {
+    id: ID
+    name: String
+    type: String
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    pet(input: PetInput): Pet
+  }
 
 `;
 
